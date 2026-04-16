3bc0ea70e1322c194fa4851f2517ab45e78808a4
Mark <brekovszkimark13@gmail.com>
2026-04-16 19:16:16 +0200

    Remove Bridge menu

--- Mini Transport Tycoon/Assets/Scripts/MenuController.cs
+++ Mini Transport Tycoon/Assets/Scripts/MenuController.cs
@@ -6,27 +6,16 @@ namespace MiniTransportTycoon
     public class MenuController : MonoBehaviour
     {
     public GameObject vehicleSubMenu;
-    public GameObject bridgeSubMenu;
 
     public void ToggleVehicleMenu()
     {
         if (vehicleSubMenu != null)
         {
             bool isCurrentlyActive = vehicleSubMenu.activeSelf;
-            bridgeSubMenu.SetActive(false);
             vehicleSubMenu.SetActive(!isCurrentlyActive);
         }
     }
 
-    public void ToggleBridgeMenu()
-    {
-        if (bridgeSubMenu != null)
-        {
-            bool isCurrentlyActive = bridgeSubMenu.activeSelf;
-            vehicleSubMenu.SetActive(false);
-            bridgeSubMenu.SetActive(!isCurrentlyActive);
-        }
-    }
 
     //Scene switch
     public void LoadNextScene(int sceneIndex)
